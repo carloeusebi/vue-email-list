83712b6a84bc4c65960a34691cbc546e355f539e
carloeusebi <carloeusebi@gmail.com>
2023-06-27 13:32:41 +0200

    Called the api and got the emails printed on page

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,24 @@ const url = "https://flynn.boolean.careers/exercises/api/random/mail";
 const app = Vue.createApp({
     data() {
         return {
-            emails: ['test@email.it']
+            numOfEmails: 10,
+            emails: []
+        }
+    },
+    methods: {
+        generateEmail() {
+            axios.get(url)
+                .then(r => {
+                    const response = r.data.response;
+                    this.emails.push(response);
+                }).catch(e => {
+                    console.error(e.message);
+                });
+        }
+    },
+    mounted() {
+        for (let i = 0; i < this.numOfEmails; i++) {
+            this.generateEmail();
         }
     }
 });
