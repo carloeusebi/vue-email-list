f078de2824bcc02f1c40c0e80420e440e60e1d8c
carloeusebi <carloeusebi@gmail.com>
2023-06-27 13:26:32 +0200

    Mounted Vue

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 const url = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+const app = Vue.createApp({
+    data() {
+        return {
+            emails: ['test@email.it']
+        }
+    }
+});
+
+app.mount('#root');
